eb6a76fcc50c35c016460be2baeaa32490ab1c36
Arman.H <ahhdev@gmail.com>
2021-11-02 09:57:30 +0330

    clear the screen for all operating systems

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use crossterm::terminal::{Clear, ClearType};
+use crossterm::{cursor, execute};
 use game_of_life::Game;
 use rand::Rng;
+use std::io::{stdout, Write};
 use std::thread;
 use std::time::Duration;
 
@@ -16,18 +19,18 @@ fn main() {
     // fill the vector that represents the game randomly with 0 and 1.
     for i in 0..game.height {
         for j in 0..game.width {
-            let random = rand::thread_rng().gen_range(0..4);
+            let random = rand::thread_rng().gen_range(0..5);
             game.world[i][j] = if random == 0 { 1 } else { 0 }
         }
     }
 
     loop {
         // clear the screen
-        print!("{}[2J", 27 as char);
+        execute!(stdout(), Clear(ClearType::All), cursor::MoveTo(0, 0)).unwrap();
         // draw the game
         game.draw();
         // sleep for 1 second
-        thread::sleep(Duration::new(1, 0));
+        thread::sleep(Duration::from_millis(800));
 
         game.evolution();
     }
